4d67fe5f62a4dfe2e70cecaa3f1fb8f8a149fec4
Pablo Siqueira <pablo16@outlook.com>
2023-05-24 17:23:27 -0300

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://ghibliapi.herokuapp.com'
+    baseURL: 'https://ghibliapi-1-v8647363.deta.app/'
 })
 
 const apiBlank = axios.create({
